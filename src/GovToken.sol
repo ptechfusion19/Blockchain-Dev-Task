ad60a1dc5e898052c3c63996de45bd9a686b9fa4
wali-hu <abdullah.se4th@gmail.com>
2025-09-29 05:58:24 -0700

    Add mint function to GovToken and create MyGovernorTest contract

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -10,6 +10,10 @@ import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 contract GovToken is ERC20, ERC20Permit, ERC20Votes {
     constructor() ERC20("GovToken", "GT") ERC20Permit("GovToken") {}
 
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+
     // The following functions are overrides required by Solidity.
 
     function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
