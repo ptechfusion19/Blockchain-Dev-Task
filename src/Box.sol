db710f1d909872d3f9a67ebe08c4bc8673aa8f9d
wali-hu <abdullah.se4th@gmail.com>
2025-09-29 01:25:28 -0700

    Fix Ownable contract initialization in Box.sol

--- src/Box.sol
+++ src/Box.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Box is Ownable(msg.sender) {
+contract Box is Ownable {
     uint256 private s_number;
 
     event NumberChanged(uint256 number);
